35acffaaa8bb7b20727b9601e04bd9cc2bfbea1a
Marc <pansmarcjoe@t-online.de>
2021-04-25 16:10:12 +0200

    .

--- client_read.py
+++ client_read.py
@@ -4,7 +4,7 @@ file_number = input('file number:')
 if file_number.isdigit():
     url = 'http://182.0.0.111/'
     try:
-        req = requests.get(f"{url}/{file_number}") 
+        req = requests.get(f"{url}{file_number}")
         print(str(req.text))
     except:
         print("server not reachable")
